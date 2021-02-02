69bd8d9fa728f13fbad41731529ef16753adc746
Robin <karlssonr1989@gmail.com>
2021-02-02 14:58:06 +0100

    commit

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import AppNavigation from "./navigation/AppNavigation";
 import AuthNavigation from "./navigation/AuthNavigation";
+import PlayerContextProvider from "./context/PlayerContext"
 
 import AuthContextProvider from "./context/AuthContext";
 
 export default function App() {
   return (
 
+    <PlayerContextProvider>
     <AuthContextProvider>
       <AuthNavigation />
     </AuthContextProvider>
+    </PlayerContextProvider>
   );
 }
 
---
+++ theme/theme.js
@@ -0,0 +1,23 @@
+
+// import { StyleSheet } from "react-native";
+
+// export const orange = ''
+
+
+//  export const theme = StyleSheet.create({
+//     colors: {
+//         orange: 's'
+//     },
+//     textType: {
+
+
+
+//     },
+//     header: {
+//       color: "white",
+//       fontSize: 60,
+//       alignSelf: "center",
+//       marginBottom: 10,
+//     },
+//   });
+
